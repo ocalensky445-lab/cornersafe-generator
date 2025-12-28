187a3f837490fea436512f671e07538c52c57fa8
Calensky-GHUB <ocalensky445@gmail.com>
2025-12-27 22:29:14 -0500

    page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 16 }}>
+      <h1>Cornersafe Generator</h1>
+      <p>Ouvre la page :</p>
+      <a href="/today">/today</a>
+    </main>
+  );
+}

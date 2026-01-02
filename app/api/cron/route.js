1361a237cf235dd6426d82281af746498b44fa88
Calensky-GHUB <ocalensky445@gmail.com>
2026-01-01 21:51:39 -0500

    Update route.js

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -4,9 +4,10 @@ import { Redis } from "@upstash/redis";
 export const dynamic = "force-dynamic";
 
 const redis = new Redis({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
+  url: process.env.KV_REST_API_URL || process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.KV_REST_API_TOKEN || process.env.UPSTASH_REDIS_REST_TOKEN,
 });
+
 
 export async function GET(request) {
   try {
